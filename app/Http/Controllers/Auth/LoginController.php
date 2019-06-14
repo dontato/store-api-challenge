df0626b23c86db26d3eba56f812f08f5f11ac5cf
Gerardo Gómez <gerardo.gomez.r@gmail.com>
2019-06-14 04:24:32 -0600

    Login endpoint

---
+++ app/Http/Controllers/Auth/LoginController.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\LoginRequest;
+use App\Http\Resources\UserResource;
+use App\Models\User;
+use Illuminate\Contracts\Auth\Factory;
+
+class LoginController extends Controller
+{
+    /**
+     * Auth factory
+     * @var \Illuminate\Contracts\Auth\Factory
+     */
+    protected $auth;
+
+    /**
+     * User model
+     * @var \App\Models\User
+     */
+    protected $users;
+
+    /**
+     * Create a new controller instance
+     * @param  \Illuminate\Contracts\Auth\Factory $auth
+     * @param  \App\Models\User                   $users
+     * @return void
+     */
+    public function __construct(Factory $auth, User $users)
+    {
+        $this->auth  = $auth;
+        $this->users = $users;
+    }
+
+    /**
+     * Perform registration
+     * @param  \App\Http\Requests\LoginRequest $request
+     * @return \App\Http\Resources\UserResource
+     */
+    public function login(LoginRequest $request)
+    {
+        $credentials = $request->validated();
+
+        if (!$token = $this->auth->attempt($credentials)) {
+            return new JsonResponse(['error' => 'Unauthorized'], 401);
+        }
+
+        return (new UserResource($this->auth->user()))
+            ->additional([
+                'meta' => [
+                    'access_token' => $token,
+                    'token_type'   => 'bearer',
+                    'expires_in'   => $this->auth->factory()->getTTL() * 60,
+                ],
+            ]);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,4 +1,9 @@
 <?php
 
 $router->post('register', 'Auth\RegisterController@register')
+    ->name('register')
+    ->middleware('guest');
+
+$router->post('login', 'Auth\LoginController@login')
+    ->name('login')
     ->middleware('guest');
