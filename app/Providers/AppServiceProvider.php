00f76b4f52d80a45286dfdd44fac02b64b6a41e0
Gerardo Gómez <gerardo.gomez.r@gmail.com>
2019-06-16 01:36:16 -0600

    Max length for string 191 to avoid index too long issues on certain mysql environments

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,6 +23,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        $this->app['db']
+            ->connection()
+            ->getSchemaBuilder()
+            ->defaultStringLength(191);
     }
 }
