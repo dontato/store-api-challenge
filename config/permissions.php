374b92f5d4a93d5ed6b2753fb8f79a744774aec5
Gerardo Gómez <gerardo.gomez.r@gmail.com>
2019-06-17 02:18:33 -0600

    Add edit any product permission

--- config/permissions.php
+++ config/permissions.php
@@ -4,6 +4,7 @@ return [
     'manage products',
     'create products',
     'edit products',
+    'edit any product',
     'update product prices',
     'update any product prices',
     'delete products',
